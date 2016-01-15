7c88839d33dcfa701f2a0732c24582c279b36d93
Jonathan <jonthomas2011@ufl.edu>
2016-01-14 21:24:02 -0500

    listings are visible and delete function works

--- listingController.js
+++ listingController.js
@@ -1,4 +1,4 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+/* angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
@@ -7,8 +7,35 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
+  /*
     $scope.addListing = function() {};
     $scope.deleteListing = function(index) {};
+    $scope.showDetails = function(index) {};
+  }
+]);
+*/
+
+/*
+   ABOVE IS ORIGINAL GIVEN CODE
+
+*/
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+  function($scope, Listings) {
+    $scope.newListing = ' ';
+    $scope.listings = Listings;
+    $scope.detailedInfo = undefined;
+
+    /* 
+      Implement these functions in the controller to make your application function 
+      as described in the assignment spec. 
+     */
+    $scope.addListing = function() {
+   //   $scope.listings.push($scope.newListing.code);
+  //  $scope.newListing = ' ';
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
     $scope.showDetails = function(index) {};
   }
 ]);
