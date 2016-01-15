57a2c4824bde4dbcacfe46daab875df8589619c5
meg-souggie <meg.souggie@ufl.edu>
2016-01-15 18:41:20 -0500

    Update listingController.js
    same as previous commit

--- listingController.js
+++ listingController.js
@@ -21,21 +21,37 @@
 */
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
-    $scope.newListing = ' ';
+    $scope.newListingCode = ' ';
+	$scope.newListingName = ' ';
+	$scope.newListingLatitude = ' ';
+	$scope.newListingLongitude = ' ';
+	$scope.newListingAddress = ' ';
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
+	$scope.showDeleteButton = false;
+	$scope.currentIndex = undefined;
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
     $scope.addListing = function() {
-   //   $scope.listings.push($scope.newListing.code);
-  //  $scope.newListing = ' ';
+		$scope.listings.push({code:$scope.newListingCode, name:$scope.newListingName, coordinates:{latitude:$scope.newListingLatitude, longitude:$scope.newListingLongitude}, address:$scope.newListingAddress});
+		$scope.newListingCode = ' ';
+		$scope.newListingName = ' ';
+		$scope.newListingLatitude = ' ';
+		$scope.newListingLongitude = ' ';
+		$scope.newListingAddress = ' ';
     };
     $scope.deleteListing = function(index) {
       $scope.listings.splice(index, 1);
+	  $scope.currentIndex = undefined;
+	  $scope.showDeleteButton = false;
+	  $scope.detailedInfo = undefined;
     };
-    $scope.showDetails = function(index) {};
+    $scope.showDetails = function(index) {
+		$scope.detailedInfo = $scope.listings[index];
+		$scope.showDeleteButton = true;
+		$scope.currentIndex = index;
+	};
   }
 ]);
